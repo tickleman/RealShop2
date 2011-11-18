bb8cb3a3c272edc8ed6e27b2eb95a2915d28349e
tickleman <mc@thgbm.net>
2011-11-18 12:34:42 +0100

    Use bukkit's ChatColor instead of Color

--- src/fr/crafter/tickleman/realplugin/RealColor.java
+++ src/fr/crafter/tickleman/realplugin/RealColor.java
@@ -1,18 +1,20 @@
 package fr.crafter.tickleman.realplugin;
 
+import org.bukkit.ChatColor;
+
 //####################################################################################### RealColor
 public class RealColor
 {
 
-	public static String cancel   = Color.LIGHT_RED;
-	public static String command  = Color.GRAY;
-	public static String doc      = Color.GRAY;
-	public static String item     = Color.LIME;
-	public static String message  = Color.LIGHT_CYAN;
-	public static String player   = Color.BROWN;
-	public static String price    = Color.YELLOW;
-	public static String quantity = Color.WHITE;
-	public static String shop     = Color.RED;
-	public static String text     = Color.GRAY;
+	public static ChatColor cancel   = ChatColor.RED;
+	public static ChatColor command  = ChatColor.GRAY;
+	public static ChatColor doc      = ChatColor.GRAY;
+	public static ChatColor item     = ChatColor.GREEN;
+	public static ChatColor message  = ChatColor.AQUA;
+	public static ChatColor player   = ChatColor.GOLD;
+	public static ChatColor price    = ChatColor.YELLOW;
+	public static ChatColor quantity = ChatColor.WHITE;
+	public static ChatColor shop     = ChatColor.RED;
+	public static ChatColor text     = ChatColor.GRAY;
 
 }
