a551be4fd46c0c7e17529bdfecd1f70409a0c88b
tickleman <mc@thgbm.net>
2011-12-01 22:47:35 +0100

    Change RealLocation.getId() form (will crash your saved shops !)

--- src/fr/crafter/tickleman/realplugin/RealLocation.java
+++ src/fr/crafter/tickleman/realplugin/RealLocation.java
@@ -38,10 +38,10 @@ public class RealLocation extends Location
 	public static String getId(Location location)
 	{
 		return 
-		+ Math.round(Math.floor(location.getX())) + ";"
+		Math.round(Math.floor(location.getX())) + ";"
 		+ Math.round(Math.floor(location.getY())) + ";"
 		+ Math.round(Math.floor(location.getZ())) + ";"
-		+ location.getWorld();
+		+ location.getWorld().getName();
 	}
 
 	//----------------------------------------------------------------------------------------- getId
